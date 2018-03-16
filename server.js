9db03ac6e0038f1bf6f7640b815f53cdc070615e
dbilderback <dbilderback@gmail.com>
2018-03-16 15:42:52 -0400

    updated for handlebars

--- server.js
+++ server.js
@@ -5,9 +5,10 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
-var passport   = require('passport')
-var session    = require('express-session')
+var passport   = require('passport');
+var session    = require('express-session');
 var bodyParser = require("body-parser");
+var exphbs = require('express-handlebars');
 
 // Sets up the Express App
 // =============================================================
@@ -17,7 +18,12 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var db = require("./models");
 
-// Sets up the Express app to handle data parsing
+//For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
