1b9e468621a0559e6c6e44c89062f7ff4fe67360
dbilderback <dbilderback@gmail.com>
2018-03-16 18:34:17 -0400

    finished implementing the authentication routine

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,9 +1,31 @@
 var authController = require('../controllers/authcontroller.js');
- 
 
-module.exports = function(app) {
- 
-    app.get('/signup', authController.signup);
-    app.get('/signin', authController.signin);
-    
-};
+module.exports = function(app,passport){
+
+app.get('/signup', authController.signup);
+
+
+app.get('/signin', authController.signin);
+
+
+app.post('/signup', passport.authenticate('local-signup',  { successRedirect: '/diary', failureRedirect: '/signup'}));
+
+
+app.get('/dashboard',isLoggedIn, authController.dashboard);
+
+
+app.get('/logout',authController.logout);
+
+
+app.post('/signin', passport.authenticate('local-signin',  { successRedirect: '/diary', failureRedirect: '/signin'}));
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/signin');
+}
+
+
+}
