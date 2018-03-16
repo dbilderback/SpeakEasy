363d6a24f5a941f71e1c80720c78fae6b4f65b80
dbilderback <dbilderback@gmail.com>
2018-03-16 15:54:21 -0400

    adding the signin page

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -5,3 +5,8 @@ exports.signup = function(req, res) {
     res.render('signup');
  
 };
+exports.signin = function(req, res) {
+ 
+    res.render('signin');
+ 
+};
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,7 +1,9 @@
 var authController = require('../controllers/authcontroller.js');
  
+
 module.exports = function(app) {
  
     app.get('/signup', authController.signup);
- 
+    app.get('/signin', authController.signin);
+    
 };
