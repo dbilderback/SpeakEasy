64a71d71a5e819e7bfad909478198fa5b45cee6e
Desmon Stanford <desmonstanford@lawn-128-61-26-235.lawn.gatech.edu>
2018-03-15 21:16:31 -0400

    revisions

--- public/js/cms.js
+++ public/js/cms.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
+  var r = document.getElementById('textBox');
+  function startConverting() {
+    if ('webkitSpeechRecognition' in window) {
+      var speechRecognizer = new webkitSpeechRecognition();
+      speechRecognizer.continuous = true;
+      speechRecognizer.interimResults = true;
+      speechRecognizer.lang = 'en-US';
+      speechRecognizer.start();
+
+      var finalTranscripts = '';
+
+      speechRecognizer.onresult = function (event) {
+        var interimTranscripts = '';
+        for (var i = 0; i < event.results.length; i++) {
+          var transcript = event.results[i][0].transcript;
+          transcript.replace('/n', '<br>');
+          if (event.results[i].isFinal) {
+            finalTranscripts += transcript;
+          } else {
+            interimTranscripts += transcript;
+          }
+        }
+        r.innerHTML = finalTranscripts + '<span style="color:#999">' + interimTranscripts +
+          '</span>';
+      };
+      speechRecognizer.onerror = function (event) {
+
+      }
+    } else {
+      r.innerHTML = 'browser not supported';
+    };
+  };
+
   // Getting jQuery references to the post body, title, form, and author select
   var bodyInput = $("#body");
   var titleInput = $("#title");
