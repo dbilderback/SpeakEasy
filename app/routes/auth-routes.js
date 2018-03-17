d5617e528fcc9a9160ed52036550a893e9f58f1e
dbilderback <dbilderback@gmail.com>
2018-03-17 12:41:08 -0400

    update to mvc and handlebars

---
+++ app/routes/auth-routes.js
@@ -0,0 +1,39 @@
+var authController = require('../controllers/authcontroller.js');
+
+module.exports = function(app,passport){
+
+app.get('/signup', authController.signup);
+
+
+app.get('/signin', authController.signin);
+
+
+//app.post('/signup', passport.authenticate('local-signup',  { successRedirect: '/diary', failureRedirect: '/signup'}));
+app.post('/signup',
+    passport.authenticate('local-signup'),
+    function(req, res) {
+        res.redirect('diary/?user_id='+req.user.userId);
+    });
+
+
+
+app.get('/logout',authController.logout);
+
+
+app.post('/signin', 
+    passport.authenticate('local-signin'), 
+        function(req, res) {
+            res.redirect('diary/?user_id'+req.user.userId);
+        });
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/signin');
+}
+
+
+}
+
