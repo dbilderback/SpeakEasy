80737f7a7129f7d65b8226eefd68510f6ba006bc
dbilderback <dbilderback@gmail.com>
2018-03-17 12:51:54 -0400

    update route files

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -5,24 +5,16 @@ exports.signup = function(req,res){
 
 	res.render('signup'); 
 
-}
+};
 
 exports.signin = function(req,res){
 
 	res.render('signin'); 
 
-}
-
-exports.dashboard = function(req,res){
-
-	res.render('dashboard'); 
-
-}
+};
 
 exports.logout = function(req,res){
-
   req.session.destroy(function(err) {
-  res.redirect('/');
+  res.redirect('/signin');
   });
-
-}
+};
