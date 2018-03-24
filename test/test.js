16290a75a9652bf06373d6c0bfaac07223e90406
Bhanoo Gulati <bhanoo.gulati@gmail.com>
2018-03-24 01:55:05 -0400

    checking in more test cases

--- test/test.js
+++ test/test.js
@@ -21,40 +21,93 @@ describe('UI Flow Tests', function() {
         })
       })
      
-    //Check Signin Functionality   - Unaouthorized User 
+    //Check Signin Functionality   - Unauthorized User 
     describe('Signin Page', function () {
-        describe('given bad data, it should state user is not authorized', () => {
-            it('should fail', done => {
+        describe('given invalid credentials', () => {
+            it('should state user is not authorized', done => {
             nightmare
             .goto('http://speakeasyappgt.herokuapp.com/signin')
             .type('input[name="email"]', 'not a valid email')
             .type('input[name="password"]', 'invalid password')
             .click('input[type="submit"]')
-            .wait(2000)
+            .wait(5000)
             .end()
-            .then()
+            .then(function(result){ done() })
             .catch(done)
             })
         })
     })
 
-    
+    //Check Create New User  - Null 
+    describe('Create Account', function () {
+        describe('given Null Data', () => {
+            it('should fail stating bad request', done => {
+            nightmare
+            .goto('http://speakeasyappgt.herokuapp.com/signup')
+            .type('input[name="firstname"]', '')
+            .click('input[type="submit"]')
+            .wait(2000)
+            .end()
+            .then(function(result){ done() })
+            .catch(done)
+            })
+        })
+    })
 
-    //Check Create Account Functionality  
-    describe('Sign In Page to Create Account Navigation', function () {
-        describe('On clicking the Create Button, it should go to Create Aaccount Page', () => {
-            it('should fail', done => {
+    //Check Create New User  - Partial Data 
+    describe('Create Account', function () {
+        describe('Given Incomplete Data', () => {
+            it('should fail stating bad request', done => {
+            nightmare
+            .goto('http://speakeasyappgt.herokuapp.com/signup')
+            .type('input[name="firstname"]', '')
+            .type('input[name="lastname"]', '')
+            .type('input[name="email"]', 'bizbuz2014@gmail.com')
+            .type('input[name="password"]', 'crazybee')
+            .click('input[type="submit"]')
+            .wait(5000)
+            .end()
+            .then(function(result){ done() })
+            .catch(done)
+            })
+        })
+    })
+
+     //Check Create New User  - Successfully
+     describe('Create Account', function () {
+        describe('Given all field inputs correctly', () => {
+            it('should create account', done => {
+            nightmare
+            .goto('http://speakeasyappgt.herokuapp.com/signup')
+            .type('input[name="firstname"]', 'Baby')
+            .type('input[name="lastname"]', 'Hack')
+            .type('input[name="email"]', 'bizbuz2014@gmail.com')
+            .type('input[name="password"]', 'crazybee')
+            .click('input[type="submit"]')
+            .wait(5000)
+            .end()
+            .then(function(result){ done() })
+            .catch(done)
+            })
+        })
+    })
+
+    //Check Signin Functionality   - Success 
+    describe('Signin Page', function () {
+        describe('given valid credentials', () => {
+            it('should navigate to users diary', done => {
             nightmare
             .goto('http://speakeasyappgt.herokuapp.com/signin')
-            .click('.btn"]')
-            .wait(2000)
+            .type('input[name="email"]', 'bizbuz2014@gmail.com')
+            .type('input[name="password"]', 'crazybee')
+            .click('input[type="submit"]')
+            .wait(5000)
             .end()
-            .then()
+            .then(function(result){ done() })
             .catch(done)
             })
         })
     })
 
     
-
 })
